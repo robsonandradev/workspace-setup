b0b9c427e7e769d090dde17ee81f936068aff529
RobsonAndradev <robsom.s.a@gmail.com>
2020-07-10 09:32:59 -0300

    Add editorconfig

--- vim-setup.sh
+++ vim-setup.sh
@@ -3,9 +3,15 @@
 echo "Starting vim setup"
 THIS=$(pwd)
 COC_EXTENSION_HOME=~/.config/coc/extensions
+COC_HOME=~/.vim/pack/coc/start
+BUNDLE_HOME=~/.vim/bundle
+EDITOR_CONFIG_HOME=~/.vim/pack/local/start
+whereWeAre='echo "Moving to $(pwd)"'
 
-mkdir -p ~/.vim
+mkdir -p $BUNDLE_HOME
 mkdir -p $COC_EXTENSION_HOME
+mkdir -p $COC_HOME
+mkdir -p $EDITOR_CONFIG_HOME
 
 echo "Setting vim commands"
 echo 'set number'     >> ~/.vimrc
@@ -14,16 +20,24 @@ echo 'autocmd vimenter * NERDTree' >> ~/.vimrc
 echo 'map <C-n> :NERDTreeToggle<CR>' >> ~/.vimrc
 mkdir -p $COC_EXTENSION_HOME
 
+cd $COC_HOME
+$whereWeAre
+echo 'Installing COC'
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+
 cp ./coc-package.json $COC_EXTENSION_HOME/package.json
 cd $COC_EXTENSION_HOME
-echo "Moved to $(pwd)"
 
+$whereWeAre
 echo "Installing coc extensions"
 npm i
 
 cd $THIS
+whereWeAre
 git clone https://github.com/scrooloose/nerdtree.git
 echo "Installing nerdtree"
-mv nerdtree ~/.vim/bundle
+mv nerdtree $BUNDLE_HOME
 
-
+echo 'Installing editorconfig'
+git clone https://github.com/editorconfig/editorconfig-vim.git
+mv editorconfig-vim $EDITOR_CONFIG_HOME
