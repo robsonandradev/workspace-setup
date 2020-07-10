f3e7f00a1816d26b93d92216438e8140cdf6c479
RobsonAndradev <robsom.s.a@gmail.com>
2020-07-10 09:58:59 -0300

    Fix folder change message

--- vim-setup.sh
+++ vim-setup.sh
@@ -6,7 +6,7 @@ COC_EXTENSION_HOME=~/.config/coc/extensions
 COC_HOME=~/.vim/pack/coc/start
 BUNDLE_HOME=~/.vim/bundle
 EDITOR_CONFIG_HOME=~/.vim/pack/local/start
-whereWeAre='echo "Moving to $(pwd)"'
+whereWeAre="echo 'Moving to '"
 
 mkdir -p $BUNDLE_HOME
 mkdir -p $COC_EXTENSION_HOME
@@ -21,19 +21,21 @@ echo 'map <C-n> :NERDTreeToggle<CR>' >> ~/.vimrc
 mkdir -p $COC_EXTENSION_HOME
 
 cd $COC_HOME
-$whereWeAre
+$whereWeAre $(pwd)
 echo 'Installing COC'
 curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
 
+cd $THIS
+$whereWeAre $(pwd)
+
 cp ./coc-package.json $COC_EXTENSION_HOME/package.json
 cd $COC_EXTENSION_HOME
-
 $whereWeAre
 echo "Installing coc extensions"
 npm i
 
 cd $THIS
-whereWeAre
+$whereWeAre $(pwd)
 git clone https://github.com/scrooloose/nerdtree.git
 echo "Installing nerdtree"
 mv nerdtree $BUNDLE_HOME
@@ -41,3 +43,4 @@ mv nerdtree $BUNDLE_HOME
 echo 'Installing editorconfig'
 git clone https://github.com/editorconfig/editorconfig-vim.git
 mv editorconfig-vim $EDITOR_CONFIG_HOME
+
